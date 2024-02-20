b729c57053571d4c446f251f70e69e71dedcc0ad
79612 <79612@DESKTOP-O1P5TC5>
2024-02-20 17:23:10 +0300

    Добавьте файлы проекта.

---
+++ поликлиника2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace поликлиника2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int visitors;
+            int waitingTimeVisitorInMinuts = 10;
+            int waitingTimeInMinuts;
+            int minutInHour;
+            int hour;
+            int overMinuts;
+
+            Console.WriteLine($"время приема посетителя:{waitingTimeVisitorInMinuts} минут");
+            Console.Write("Сколько человек в очереди?");
+            visitors = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("сколько минут в одном часе?");
+            minutInHour = Convert.ToInt32(Console.ReadLine());
+
+            waitingTimeInMinuts = visitors * waitingTimeVisitorInMinuts;
+            hour = waitingTimeInMinuts / minutInHour;
+            overMinuts = waitingTimeInMinuts % minutInHour;
+
+            Console.WriteLine($"время ожидания {hour} часов и {overMinuts} минут");
+        }
+    }
+}
